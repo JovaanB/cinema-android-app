38ba3092d6a2a81a783e52f343407a5142cd1bad
Jovan Bienvenu <jbienvenu@wisersoftware.com>
2021-09-23 16:51:18 +0200

    Ahh auth

---
+++ app/src/main/java/models/Reviews.java
@@ -0,0 +1,14 @@
+package models;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Reviews {
+    private String title;
+    private Date date;
+    private Time heure;
+    private Float note_script;
+    private Float note_realisation;
+    private Float note_music;
+    private String text;
+}
